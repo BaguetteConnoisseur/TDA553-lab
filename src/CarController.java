e3c2631e8a6fb6f6797475c2799790f6531f8709
Ponky <77840376+BaguetteConnoisseur@users.noreply.github.com>
2023-12-06 14:03:20 +0100

    Import Math

--- src/CarController.java
+++ src/CarController.java
@@ -3,7 +3,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
-
+import java.math.*;
 
 /*
 * This class represents the Controller part in the MVC pattern.
